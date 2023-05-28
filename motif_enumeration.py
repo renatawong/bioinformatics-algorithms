e7eb18d77a5e482abc1cb0dd648abe8ee75007a7
Renata Wong <50943402+renatawong@users.noreply.github.com>
2023-05-28 16:48:24 +0800

    A brute force method for finding motifs of particular length and mutation in a set of DNA sequences

--- motif_enumeration.py
+++ motif_enumeration.py
@@ -9,28 +9,79 @@ the expression of different genes (circadian activity).
 This is a brute-force method. 
 
 The file to be read consists of 2 lines:
-Line 1: genome sequence
-Line 2: k-mer
-Example: Vibrio_cholerae.txt, the DNA sequence of the bacteria that couses cholera.
+Line 1: k d
+Line 2: sequence1 sequence2 sequence3 ...
+
+If no file is specified, 'Motifs.txt' is loaded by default.
 '''
 
 import sys
 
 
 
+
+
+NUCLEOTIDES = ['A', 'T', 'G', 'C']
+
+
+def hamming_distance(p, q):
+
+    hamming_distance = 0
+
+    for p_i, q_i in zip(p, q):
+        if p_i != q_i:
+            hamming_distance += 1
+
+    return hamming_distance
+
+
+
+
+def get_suffix(pattern):
+
+    return pattern[1:]
+
+
+
+
+
+def get_neighbours(pattern, d):
+
+    if d == 0:
+        return {pattern}
+    if len(pattern) == 1: 
+        return {'A', 'C', 'G', 'T'}
+    
+    neighbourhood = set()
+    
+    suffix_neighbors = get_neighbours(get_suffix(pattern), d)
+
+    for suffix in suffix_neighbors:
+        if hamming_distance(get_suffix(pattern), suffix) < d:
+            for base in NUCLEOTIDES:
+                neighbourhood.add(base+suffix)
+        else:
+            neighbourhood.add(pattern[0]+suffix)
+        
+    return neighbourhood
+
+
+
 def motif_enumeration(sequences, k, d):
-    kmer_list = [set(s) for s in sequences] # Creating a list of sets
+
+    kmer_list = [set() for _ in sequences] 
     
     for index, seq in enumerate(sequences):
         for k_pos in range(len(seq) - k + 1):
-            # Generate neighbors for all kmers in all strings and
-
-            # add them to a set() in a kmer_list
+            # Generate neighbors for each kmer in a sequence
+            kmer = seq[k_pos : k_pos + k]
+            kmer_list[index].update(get_neighbours(kmer, d))
     
     motifs = kmer_list[0]
-    #for k_set in kmer_list:
-        # Use 'AND' on a Python set() to ONLY get kmers from all sets
-        # that are in all sets we generated above
+    
+    for k_set in kmer_list[1:len(kmer_list)]:
+        # Take the intersection of all sets
+        motifs = motifs.intersection(k_set)      # the result of intersection() must be stored in a new object. 
 
     return motifs
 
@@ -40,11 +91,10 @@ def motif_enumeration(sequences, k, d):
 def compute(f):
     k, d = map(int, f.readline().split())
     sequences = [seq for seq in map(str, f.readline().split())]
-    print(sequences)
 
     motifs = motif_enumeration(sequences, k, d)
-    print('({},{})-motifs found: '.format(k, d)) 
-    print(motifs)
+    print('({},{})-motifs found in all sequences: '.format(k, d)) 
+    print(*motifs)
 
     f.close()
 
@@ -61,7 +111,7 @@ if __name__ == "__main__":
 
     # For execution from IDE
     if len(sys.argv) == 1:
-        with open('Vibrio_cholerae.txt', 'r') as genome_file:
+        with open('Motifs.txt', 'r') as genome_file:
             compute(genome_file)
 
     # For execution from terminal
